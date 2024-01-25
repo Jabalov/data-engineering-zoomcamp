1ae29fae252cf424e8a05974b31fa095cb03eb6b
Jabal <moh.aboajabl@gmail.com>
2024-01-25 23:20:18 +0000

    push solutions

--- week_1_basics_n_setup/1_terraform_gcp/terraform/terraform_with_variables/variables.tf
+++ week_1_basics_n_setup/1_terraform_gcp/terraform/terraform_with_variables/variables.tf
@@ -8,7 +8,7 @@ variable "credentials" {
 
 variable "project" {
   description = "Project"
-  default     = "<Your Project ID>"
+  default     = "taxi-rides-ny-409108"
 }
 
 variable "region" {
@@ -32,7 +32,7 @@ variable "bq_dataset_name" {
 variable "gcs_bucket_name" {
   description = "My Storage Bucket Name"
   #Update the below to a unique bucket name
-  default     = "terraform-demo-terra-bucket"
+  default     = "taxi-rides-ny-409108-terra-bucket"
 }
 
 variable "gcs_storage_class" {
--- week_1_basics_n_setup/2_docker_sql/ingest_data.py
+++ week_1_basics_n_setup/2_docker_sql/ingest_data.py
@@ -42,24 +42,16 @@ def main(params):
     df.to_sql(name=table_name, con=engine, if_exists='append')
 
 
-    while True: 
-
+    while True:
         try:
             t_start = time()
-            
             df = next(df_iter)
-
-            df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
-            df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
-
-            df.to_sql(name=table_name, con=engine, if_exists='append')
-
+            df.lpep_pickup_datetime = pd.to_datetime(df.lpep_pickup_datetime)
+            df.lpep_dropoff_datetime = pd.to_datetime(df.lpep_dropoff_datetime)
+            df.to_sql(name='green_taxi_data', con=engine, if_exists='append')
             t_end = time()
-
             print('inserted another chunk, took %.3f second' % (t_end - t_start))
-
         except StopIteration:
-            print("Finished ingesting data into the postgres database")
             break
 
 if __name__ == '__main__':
